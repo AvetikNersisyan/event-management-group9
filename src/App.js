fac224f5ff80f238350e168b4bca6a1c6b3f67aa
Tatev84 <tatevh84@gmail.com>
2022-02-06 21:08:13 +0400

    categories

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
-import './App.css';
-import {Routes, Route} from "react-router-dom";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import Profile from "./components/profile";
 import Events from "./components/events";
 import Header from "./components/header/header";
-import {useEffect} from "react";
-import {fetchData} from "./redux/ducks/eventDuck";
-import {useDispatch} from "react-redux";
-import {setLoggedInUser} from "./redux/userDuck";
-
+import Categories from "./components/categories";
+import { useEffect } from "react";
+import { fetchData } from "./redux/ducks/eventDuck";
+import { useDispatch } from "react-redux";
+import { setLoggedInUser } from "./redux/userDuck";
 
 function App() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/1")
+      .then((res) => res.json())
+      .then((res) => dispatch(setLoggedInUser(res)));
 
+    // fake user, to be deleted later
+  }, []);
 
-
-
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users/1")
-            .then(res => res.json())
-            .then(res =>  dispatch(setLoggedInUser(res)));
-
-
-        // fake user, to be deleted later
-
-
-    }, []);
-
-    return (
-        <div className="App">
-            <Header/>
-            <Routes>
-                <Route path={"/"} element={<Home/>}/>
-                <Route path={"/profile"} element={<Profile/>}/>
-                <Route path={"/events"} element={<Events/>}/>
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/profile"} element={<Profile />} />
+        <Route path={"/events"} element={<Events />} />
+        <Route path={"/categories"} element={<Categories />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,6 +6,7 @@ const Header = () => {
             <NavLink to={"/"}> Home </NavLink>
             <NavLink to={"/events"}> Events </NavLink>
             <NavLink to={"/profile"}> Profile </NavLink>
+            <NavLink to={"/categories"}> Categories </NavLink>
         </div>
     )
 }
