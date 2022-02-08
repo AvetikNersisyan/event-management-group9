dc5df15f0775f394d94f4a76ad53f1a81cb7f400
Avetik Nersisyan <88989924+AvetikNersisyan@users.noreply.github.com>
2022-02-08 14:11:02 +0400

    Avatar url fixed

--- src/components/profile/profile/index.js
+++ src/components/profile/profile/index.js
@@ -1,46 +1,48 @@
 import React from 'react';
+import AvatarURL from '../../../assets/img/avatar.png';
+
 const ProfileInfo = ({ activeUser, setActiveUser }) => {
-    const handleLogOut = () => {
-        setActiveUser(null);
-    };
-    return (
-        <div className='myProfile'>
-            <div className='profilePhoto'>
-                <img className='photo' src='/img/avatar.png' alt={'#'} />
-            </div>
-            <div className='about'>
-                <p>
-                    {activeUser.firstname} {activeUser.lastname}
-                </p>
-                <div className='interests'>
-                    <p className='interests'>Interestes</p>
-                    <div className='interestsItems'>
-                        {activeUser.interests.map((e) => (
-                            <span className='interestsItem'>{e}</span>
-                        ))}
-                    </div>
-                </div>
-            </div>
-            <div className='events'>
-                <p>Interested</p>
-                {activeUser.interestedEvents.map((e) => (
-                    <div className='event'>{e}</div>
-                ))}
-            </div>
-            <div className='going'>
-                <p>Going</p>
-                {activeUser.going.map((e) => (
-                    <div className='event'>{e}</div>
-                ))}
-            </div>
-            <div className='going'>
-                <p>Allready gone</p>
-                {activeUser.allreadyGone.map((e) => (
-                    <div className='event'>{e}</div>
-                ))}
-            </div>
-            <button onClick={handleLogOut}>LogOut</button>
-        </div>
-    );
+	const handleLogOut = () => {
+		setActiveUser(null);
+	};
+	return (
+		<div className='myProfile'>
+			<div className='profilePhoto'>
+				<img className='photo' src={AvatarURL} alt={'#'} />
+			</div>
+			<div className='about'>
+				<p>
+					{activeUser.firstname} {activeUser.lastname}
+				</p>
+				<div className='interests'>
+					<p className='interests'>Interestes</p>
+					<div className='interestsItems'>
+						{activeUser.interests.map((e) => (
+							<span className='interestsItem'>{e}</span>
+						))}
+					</div>
+				</div>
+			</div>
+			<div className='events'>
+				<p>Interested</p>
+				{activeUser.interestedEvents.map((e) => (
+					<div className='event'>{e}</div>
+				))}
+			</div>
+			<div className='going'>
+				<p>Going</p>
+				{activeUser.going.map((e) => (
+					<div className='event'>{e}</div>
+				))}
+			</div>
+			<div className='going'>
+				<p>Allready gone</p>
+				{activeUser.allreadyGone.map((e) => (
+					<div className='event'>{e}</div>
+				))}
+			</div>
+			<button onClick={handleLogOut}>LogOut</button>
+		</div>
+	);
 };
 export default ProfileInfo;
