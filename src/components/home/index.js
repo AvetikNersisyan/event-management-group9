4c015457fe15dd0175800ba7d75289fc80731286
Abbafaria <nersisyan.avetik@gmail.com>
2022-02-25 15:28:18 +0400

    homepage filter removed

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,28 +1,14 @@
-import Search from './search/index';
 import './index.css';
-/* import Populiar from './populiar';
- import Featured from './featured';
-import Review from '../review';  */
+
 import Feedback from '../feedback';
 import Slider from './slider';
 import Countdown from '../countdown';
 
-import Categories from '../categories'
-
 const Home = () => {
 	return (
-		<div /* className='global-container' */>
-			{/* <div className='search_wrapper'>
-				<Search />;
-			</div> */}
-			{/* <Populiar />
-			<Featured />
-			<Review />  */}
-			<Categories />
+		<div>
 			<Slider />
 			<Countdown />
-
-
 			<Feedback />
 		</div>
 	);
